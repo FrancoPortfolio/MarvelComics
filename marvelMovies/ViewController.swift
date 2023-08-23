2200b642f86870ead6697fed0567e5046453a231
Franco Marquez <20191218@aloe.ulima.edu.pe>
2023-08-23 09:46:29 -0500

    Initial Commit

---
+++ marvelMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  marvelMovies
+//
+//  Created by Franco Marquez on 23/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
